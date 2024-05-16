2a05706c61ea763fa136020a2f37df57e091ddd2
Teya Urena <teyaleoni@gmail.com>
2024-05-15 19:28:25 -0700

    added blog

---
+++ src/pages/blog/index.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+export default () => {
+  return (
+    <div>
+      linda
+      <a href="./why-pilates-is-worth-it">why-pilates-is-worth-it</a>
+    </div>
+  );
+};
