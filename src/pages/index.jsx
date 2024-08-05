8945226265592ccd234ed558b717fac95b158aed
Teya Urena <teyaleoni@gmail.com>
2024-08-04 21:28:00 -0700

    rearranged site

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -40,6 +40,7 @@ const IndexPage = () => (
         </p>
       </div>
     </section>
+   
     <section id="about" className="grid-row">
       <div className="about-picture">
         <img src={aboutImg} alt="me" width="100%" />
@@ -76,6 +77,67 @@ const IndexPage = () => (
           invest in the well-being of our future selves. Because your future
           self deserves the very best!
         </p>
+      </div>
+    </section>
+    <section id="testimonials">
+      <h1>What my clients are saying...</h1>
+      <div className="grid-row-lg">
+        <div>
+          <h2>Annabelle B.</h2>
+          <p>
+            Teya has truly been a game-changer in my life. I came to her with
+            persistent knee and back injuries that had haunted me for over a
+            decade. Despite seeking help from doctors and massages, nothing
+            provided lasting relief until I began consistently attending Teya's
+            Pilates sessions. Teya's approach is nothing short of remarkable.
+            She exudes patience, understanding the unique challenges each
+            individual faces. What sets her apart is her ability to create a
+            judgment-free environment, ensuring I never felt discouraged when
+            struggling to grasp certain exercises. This encouragement played a
+            pivotal role in building my confidence and commitment to the
+            practice. The impact on my physical well-being has been astonishing.
+            I now live pain-free, something I hadn't thought possible after
+            years of discomfort. The strength and flexibility I've gained
+            through Pilates have not only alleviated my injuries but have also
+            empowered me to navigate my daily activities and work without the
+            burden of constant pain. If you want to change your life for the
+            better, go to Teya.
+          </p>
+        </div>
+        <div>
+          <h2>Maryloie E.</h2>
+          <p>
+            Teya is a huge contribution to my pregnancy journey. Going to her
+            twice a week set me up for success during delivery. After seeing her
+            every session my body felt stronger and help alleviate pain and
+            strengthened my body to carry my beautiful baby. She listened to my
+            concerns and pain points and tailored each session to what my body
+            needed. Teya is passionate about her craft and cares deeply about
+            her clients. She is collaborative and caring. I look forward to each
+            session with her.
+          </p>
+        </div>
+        <div>
+          <h2>Chelsea W.</h2>
+          <p>
+            Training will Teya is a treat! I suffer from a lot of muscle
+            imbalances due to past injuries. Teya helped me strengthen and
+            stretch those areas that so desperately needed it. She explains
+            which muscles we are targeting in each exercise. She also will fix
+            your form like no other (something you'll never get in a group
+            pilates class). Taking pilates lessons will Teya has been amazing
+            for my body!
+          </p>
+        </div>
+        <div>
+          <h2>Skyler B.</h2>
+          <p>
+            While training with Teya, she was able to explain why the workouts
+            were beneficial for my needs. I appreciated her patience and that
+            each workout was modified/tailored to help me get in the best
+            workout for my level. She is an excellent pilates instructor!
+          </p>
+        </div>
       </div>
     </section>
     <section id="quote">
@@ -234,6 +296,12 @@ const IndexPage = () => (
           </p>
         </Faq>
 
+        <Faq question="How do I book a session?">
+          <p>
+            Please contact me at info@stancepilates.com or text/call 702-608-2065. Looking forward to hearing from you!
+          </p>
+        </Faq>
+
         <Faq question="Do you sell Pilates gift certificates?">
           <p>
             Absolutely! We're all about spreading the Pilates love with our
@@ -248,67 +316,6 @@ const IndexPage = () => (
             Rd #200B, Las Vegas, NV 89148
           </p>
         </Faq>
-      </div>
-    </section>
-    <section id="testimonials">
-      <h1>Testimonials</h1>
-      <div className="grid-row-lg">
-        <div>
-          <h2>Annabelle B.</h2>
-          <p>
-            Teya has truly been a game-changer in my life. I came to her with
-            persistent knee and back injuries that had haunted me for over a
-            decade. Despite seeking help from doctors and massages, nothing
-            provided lasting relief until I began consistently attending Teya's
-            Pilates sessions. Teya's approach is nothing short of remarkable.
-            She exudes patience, understanding the unique challenges each
-            individual faces. What sets her apart is her ability to create a
-            judgment-free environment, ensuring I never felt discouraged when
-            struggling to grasp certain exercises. This encouragement played a
-            pivotal role in building my confidence and commitment to the
-            practice. The impact on my physical well-being has been astonishing.
-            I now live pain-free, something I hadn't thought possible after
-            years of discomfort. The strength and flexibility I've gained
-            through Pilates have not only alleviated my injuries but have also
-            empowered me to navigate my daily activities and work without the
-            burden of constant pain. If you want to change your life for the
-            better, go to Teya.
-          </p>
-        </div>
-        <div>
-          <h2>Maryloie E.</h2>
-          <p>
-            Teya is a huge contribution to my pregnancy journey. Going to her
-            twice a week set me up for success during delivery. After seeing her
-            every session my body felt stronger and help alleviate pain and
-            strengthened my body to carry my beautiful baby. She listened to my
-            concerns and pain points and tailored each session to what my body
-            needed. Teya is passionate about her craft and cares deeply about
-            her clients. She is collaborative and caring. I look forward to each
-            session with her.
-          </p>
-        </div>
-        <div>
-          <h2>Chelsea W.</h2>
-          <p>
-            Training will Teya is a treat! I suffer from a lot of muscle
-            imbalances due to past injuries. Teya helped me strengthen and
-            stretch those areas that so desperately needed it. She explains
-            which muscles we are targeting in each exercise. She also will fix
-            your form like no other (something you'll never get in a group
-            pilates class). Taking pilates lessons will Teya has been amazing
-            for my body!
-          </p>
-        </div>
-        <div>
-          <h2>Skyler B.</h2>
-          <p>
-            While training with Teya, she was able to explain why the workouts
-            were beneficial for my needs. I appreciated her patience and that
-            each workout was modified/tailored to help me get in the best
-            workout for my level. She is an excellent pilates instructor!
-          </p>
-        </div>
       </div>
     </section>
     <section id="contact">
