39b1fa49dee11cd7ceaacb1f8af29f6b3389db9b
Teya Lorts <teyaleoni@gmail.com>
2025-01-31 19:45:57 -0800

    flodesk

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -5,6 +5,34 @@ import parallelEventImage from "../images/parallel-event.png";
 import uncommonsImage from "../images/uncommons-event.png";
 import tbaImage from "../images/tba.png";
 
+export const Head = () => (
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+      (function(w, d, t, h, s, n) {
+        w.FlodeskObject = n;
+        var fn = function() {
+          (w[n].q = w[n].q || []).push(arguments);
+        };
+        w[n] = w[n] || fn;
+        var f = d.getElementsByTagName(t)[0];
+        var v = '?v=' + Math.floor(new Date().getTime() / (120 * 1000)) * 60;
+        var sm = d.createElement(t);
+        sm.async = true;
+        sm.type = 'module';
+        sm.src = h + s + '.mjs' + v;
+        f.parentNode.insertBefore(sm, f);
+        var sn = d.createElement(t);
+        sn.async = true;
+        sn.noModule = true;
+        sn.src = h + s + '.js' + v;
+        f.parentNode.insertBefore(sn, f);
+      })(window, document, 'script', 'https://assets.flodesk.com', '/universal', 'fd');
+    `,
+    }}
+  />
+);
+
 export default function events() {
   return (
     <Page navHrefPrefix="/">
@@ -25,8 +53,8 @@ export default function events() {
               <br />
               ⏰ 11:00AM
               <br />
-              📍 The Assembly by Kiln at Uncommons | 8460 Rozita Lee Ave Las Vegas,
-              NV 89113
+              📍 The Assembly by Kiln at Uncommons | 8460 Rozita Lee Ave Las
+              Vegas, NV 89113
               <br />
               🧘‍♀️ Mats will be provided
             </p>
@@ -55,6 +83,17 @@ export default function events() {
             </p>
           </div>
         </div>
+        <div id="fd-form-679d92fe97bfd787c5a1da9c"></div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.fd('form', {
+                formId: '679d92fe97bfd787c5a1da9c',
+                containerEl: '#fd-form-679d92fe97bfd787c5a1da9c'
+              });
+            `,
+          }}
+        />
       </section>
       <section className="content-block">
         <h2>Previous Events</h2>
