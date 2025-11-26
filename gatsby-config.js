10255da47f23e259ae15e023fb75b28c1c7a705f
Teya Lorts <teyaleoni@gmail.com>
2025-11-25 16:29:39 -0800

    update seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `gatsby-stance-site`,
+    title: `Stance Pilates | Private Studio Southwest Las Vegas`,
+    description: `Exclusive 1-on-1 training in Southwest Las Vegas (89113). Personalized sessions using the Pilates Reformer and Cadillac for strength, back pain relief, and control.`,
     siteUrl: `https://stancepilates.com`,
   },
   plugins: [
